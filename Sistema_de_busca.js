7e68de39b9db26706e3c2baed51cbf82d0fd48ad
luancavalcantiribeiro <89954911+luancavalcantiribeiro@users.noreply.github.com>
2021-09-01 23:33:53 -0300

    Create Sistema_de_busca.js

---
+++ Sistema_de_busca.js
@@ -0,0 +1,2 @@
+<script src="sistema_de_busca.js">
+pesquisa_input = document.querySelectorAll(".pesquisa"); 	for(i in pesquisa_input){ 		 			pesquisa_input[i].onkeyup=function(e){ 				reg = new RegExp(this.value.toLowerCase(),"g") 				lis = this.parentElement.querySelector(".lista") 				console.log(lis) 				for(j of lis.children){ 					if( !j.getAttribute("nome").match(reg) ) 						j.style.display="none" 					else 						j.removeAttribute("style") 				} 			} 	}

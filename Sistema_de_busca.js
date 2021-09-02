cb8f22fd8e6a6f3896a1b72d83eac99e1a1a322c
luancavalcantiribeiro <89954911+luancavalcantiribeiro@users.noreply.github.com>
2021-09-01 23:44:31 -0300

    Update Sistema_de_busca.js

--- Sistema_de_busca.js
+++ Sistema_de_busca.js
@@ -2,7 +2,7 @@
 pesquisa_input = document.querySelectorAll(".pesquisa"); 	for(i in pesquisa_input){ 		 			pesquisa_input[i].onkeyup=function(e){ 				reg = new RegExp(this.value.toLowerCase(),"g") 				lis = this.parentElement.querySelector(".lista") 				console.log(lis) 				for(j of lis.children){ 					if( !j.getAttribute("nome").match(reg) ) 						j.style.display="none" 					else 						j.removeAttribute("style") 				} 			} 	}
 <div class="h-100-p">
 
-	<input class="pesquisa" placeholder="Escreva aqui">
+	<input class="pesquisa" placeholder="index.html">
 
 	<ul class="lista">
 
